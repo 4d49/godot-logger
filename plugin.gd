79b97207914d8c6b3c3c7ed16a9ab5e3846fdb28
Mansur Isaev <737dab2f169a@mail.ru>
2024-03-27 10:36:53 +0400

    Replace `Singletone` by static methods

--- plugin.gd
+++ plugin.gd
@@ -5,11 +5,8 @@
 extends EditorPlugin
 
 
-const AUTOLOAD_NAME = "Log"
-const AUTOLOAD_PATH = "res://addons/godot-logger/scripts/logger.gd"
-
-const LOGGER_OUTPUT = "LoggerOutput"
-const LOGGER_OUTPUT_SCRIPT = "res://addons/godot-logger/scripts/logger_output.gd"
+const LOGGER_OUTPUT = "LogOutput"
+const LOGGER_OUTPUT_SCRIPT = "res://addons/godot-logger/scripts/log_output.gd"
 const LOGGER_OUTPUT_ICON = "res://addons/godot-logger/icons/log_output.svg"
 
 
@@ -40,10 +37,8 @@ func _enter_tree() -> void:
 	_def_settings("plugins/logger/default_output_format", "[{hour}:{minute}:{second}][{level}]{text}")
 	_def_settings("plugins/logger/file_format", "[{hour}:{minute}:{second}][{level}]{text}")
 
-	add_autoload_singleton(AUTOLOAD_NAME, AUTOLOAD_PATH)
 	add_custom_type(LOGGER_OUTPUT, "RichTextLabel", load(LOGGER_OUTPUT_SCRIPT), load(LOGGER_OUTPUT_ICON))
 
 
 func _exit_tree() -> void:
 	remove_custom_type(LOGGER_OUTPUT)
-	remove_autoload_singleton(AUTOLOAD_NAME)
--- scripts/log_output.gd
+++ scripts/log_output.gd
@@ -1,59 +1,37 @@
 # Copyright (c) 2020-2024 Mansur Isaev and contributors - MIT License
 # See `LICENSE.md` included in the source distribution for details.
 
-@tool
-## LoggerOutput class.
-class_name LoggerOutput
+## LogOutput class.
+class_name LogOutput
 extends RichTextLabel
 
-## Emited when handle [signal Logger.logged] signal.
+## Emited when handle [signal Log.logged] signal.
 signal handled()
 
 
 @export var format_text : String = "[{hour}:{minute}:{second}][{level}]{text}"
 @export var colors : Dictionary = {
-	Logger.INFO: Color.WHITE,
-	Logger.DEBUG: Color.GRAY,
-	Logger.WARNING: Color.YELLOW,
-	Logger.ERROR: Color.RED,
-	Logger.FATAL: Color.RED,
+	Log.INFO: Color.WHITE,
+	Log.DEBUG: Color.GRAY,
+	Log.WARNING: Color.YELLOW,
+	Log.ERROR: Color.RED,
+	Log.FATAL: Color.RED,
 }
 
 
-var _logger : Logger
-
-
 func _enter_tree() -> void:
-	var logger := get_node_or_null("/root/Log") as Logger
-	if is_instance_valid(logger):
-		set_logger(logger)
+	if not (Log as Object).is_connected(&"logged", handle_message):
+		var error: Error = (Log as Object).connect(&"logged", handle_message)
+		assert(error == OK, error_string(error))
 
 
 func _exit_tree() -> void:
-	if is_instance_valid(_logger) and _logger.logged.is_connected(handle_message):
-		_logger.logged.disconnect(handle_message)
+	if (Log as Object).is_connected(&"logged", handle_message):
+		(Log as Object).disconnect(&"logged", handle_message)
 
 ## Return the level color or [member Color.WHITE].
 func get_color(level: int) -> Color:
 	return colors.get(level, Color.WHITE)
-
-## Set the logger.
-func set_logger(logger: Logger) -> void:
-	assert(is_instance_valid(logger), "Invalid Logger.")
-	if not is_instance_valid(logger) or is_same(_logger, logger):
-		return
-
-	if is_instance_valid(_logger) and _logger.logged.is_connected(handle_message):
-		_logger.logged.disconnect(handle_message)
-
-	if not logger.logged.is_connected(handle_message):
-		logger.logged.connect(handle_message)
-
-	_logger = logger
-
-## Return the logger.
-func get_logger() -> Logger:
-	return _logger
 
 ## Return formatted string from message.
 func format(message: Dictionary) -> String:
